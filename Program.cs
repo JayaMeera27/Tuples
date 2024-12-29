91353f1d78f4ab86e0ced3e1a8c82ea83573c8e6
Jaya Meera B <jayameera272@outlook.com>
2024-12-29 15:15:16 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Sample
+{
+    public Tuple<string,int> GetDetails()
+    {
+        Tuple<string,int> tuple = new Tuple<string,int>("Jaya",26);
+        return tuple;
+    }
+}
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Tuple<string, int> person = new Tuple<string, int>("Meera", 27);
+        Console.WriteLine(person); // ("Meera", 27)
+        Console.WriteLine(person.Item1);
+
+
+        Sample s = new Sample();
+        Tuple<string, int> getperson = s.GetDetails();
+        Console.WriteLine(getperson.Item1);
+        Console.WriteLine(getperson.Item2);
+
+    }
+}
